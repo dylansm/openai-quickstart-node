a19390e5dd34766cdac111447add99f83ac2f705
Dylan Smith <rh@izo.me>
2023-05-05 15:37:18 -0700

    First commit

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -9,30 +9,44 @@ export default async function (req, res) {
   if (!configuration.apiKey) {
     res.status(500).json({
       error: {
-        message: "OpenAI API key not configured, please follow instructions in README.md",
-      }
+        message:
+          "OpenAI API key not configured, please follow instructions in README.md",
+      },
     });
     return;
   }
 
-  const animal = req.body.animal || '';
+  const animal = req.body.animal || "";
   if (animal.trim().length === 0) {
     res.status(400).json({
       error: {
         message: "Please enter a valid animal",
-      }
+      },
     });
     return;
   }
 
   try {
-    const completion = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: generatePrompt(animal),
+    //const completion = await openai.createCompletion({
+    //model: "text-davinci-003",
+    //prompt: generatePrompt(animal),
+    //temperature: 0.6,
+    //});
+    const completion = await openai.createChatCompletion({
+      // use either gpt-3.5-turbo or gpt-4
+      model: "gpt-3.5-turbo",
+      //model: "gpt-4",
+      messages: [{ role: "user", content: generatePrompt(animal) }],
       temperature: 0.6,
     });
-    res.status(200).json({ result: completion.data.choices[0].text });
-  } catch(error) {
+    // data is accessed differently depending on the API used
+    console.log(completion.data.choices[0].message.content);
+    //console.log(completion.data.choices[0].text);
+    res
+      .status(200)
+      .json({ result: completion.data.choices[0].message.content });
+    //res.status(200).json({ result: completion.data.choices[0].text });
+  } catch (error) {
     // Consider adjusting the error handling logic for your use case
     if (error.response) {
       console.error(error.response.status, error.response.data);
@@ -41,8 +55,8 @@ export default async function (req, res) {
       console.error(`Error with OpenAI API request: ${error.message}`);
       res.status(500).json({
         error: {
-          message: 'An error occurred during your request.',
-        }
+          message: "An error occurred during your request.",
+        },
       });
     }
   }
